355bf0658f13086972bf2da3c9f2ff12ee89f32d
HamidFt <hamidfattahi.a@gmail.com>
2023-12-07 19:34:33 +0330

    category & filter state manage in Url Query (AppRouter) Fixes #3

--- src/app/_components/filters/filters.tsx
+++ src/app/_components/filters/filters.tsx
@@ -1,25 +1,48 @@
 import { Filter } from "@/types/product-filter.type";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 type FiltersProps = {
   filters: Filter[];
+  selectedFilters: { filter: number; option: number }[];
   setSelectedFilters: React.Dispatch<
     React.SetStateAction<{ filter: number; option: number }[]>
   >;
 };
 
-const Filters: React.FC<FiltersProps> = ({ filters, setSelectedFilters }) => {
+const Filters: React.FC<FiltersProps> = ({
+  filters,
+  selectedFilters,
+  setSelectedFilters,
+}) => {
+  const { replace } = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  function handleUrl(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("filter", term);
+    } else {
+      params.delete("filter");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const filterId = Number(event.target.name.split("-")[1]);
     const optionId = Number(event.target.value);
     if (event.target.checked) {
-      setSelectedFilters((prev) => [
-        ...prev,
-        { filter: filterId, option: optionId },
-      ]);
+      setSelectedFilters((prev) => {
+        const newFilters = [...prev, { filter: filterId, option: optionId }];
+        handleUrl(newFilters.map((f) => `${f.filter}-${f.option}`).join(","));
+        return newFilters;
+      });
     } else {
-      setSelectedFilters((prev) =>
-        prev.filter((f) => f.filter !== filterId || f.option !== optionId)
-      );
+      setSelectedFilters((prev) => {
+        const newFilters = prev.filter(
+          (f) => f.filter !== filterId || f.option !== optionId
+        );
+        handleUrl(newFilters.map((f) => `${f.filter}-${f.option}`).join(","));
+        return newFilters;
+      });
     }
   };
   return (
@@ -38,6 +61,11 @@ const Filters: React.FC<FiltersProps> = ({ filters, setSelectedFilters }) => {
                     id={option.OptionName}
                     value={option.OptionID}
                     onChange={handleCheckboxChange}
+                    checked={selectedFilters.some(
+                      (f) =>
+                        f.filter === filter.FilterID &&
+                        f.option === option.OptionID
+                    )}
                   />
                   {option.OptionName}
                 </label>
